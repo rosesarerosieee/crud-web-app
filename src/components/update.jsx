63a5a4136356c279ba9fc841519b970026b6f038
rosesarerosieee <perezrose0211@gmail.com>
2024-10-12 21:18:42 +0800

    Fixed the update section

--- src/components/update.jsx
+++ src/components/update.jsx
@@ -24,35 +24,47 @@ const UpdateUser = () => {
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center">
-        <div className="flex items-center justify-start flex-col w-[300px] h-[50vh] bg-[pink]">
-          <h2>Update User</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="User ID"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              required
-            />
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Age"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-            />
-            <button type="submit">Update User</button>
+        <div className="flex items-center justify-start flex-col w-[300px] h-[50vh] bg-[whitesmoke]">
+          <h2 className="font-extrabold text-[2rem]">Update User</h2>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center justify-center"
+          >
+            <div className="flex flex-col items-center justify-center">
+              <input
+                type="text"
+                placeholder="User ID"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="pt-[30px]">
+              <button
+                type="submit"
+                className="w-[150px] h-[50px] text-center rounded-[5px]  bg-[whitesmoke]"
+              >
+                Update User
+              </button>
+            </div>
           </form>
         </div>
       </div>
